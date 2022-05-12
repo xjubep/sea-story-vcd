af24b013d9c849a41c7d78a443d44fc9da5f5e65
xjubep <xjubep@gmail.com>
2022-05-12 12:37:33 +0900

    fix video list load error

--- decode_video.py
+++ decode_video.py
@@ -1,28 +1,46 @@
 import argparse
 import os
-import tqdm
-from VCD.utils import *
+import subprocess
 from datetime import datetime
+
+import tqdm
+
+from VCD.utils import *
 
 logger = None
 
 
+def ffmpeg_subprocess(cmd):
+    p = subprocess.Popen(args=cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
+    _ = p.communicate()
+
+
 def extract_frame(video, frame_dir, decode_rate):
-    cmd = f'ffmpeg -ss 00:00:0 -i "{video}" -r {decode_rate} -f image2 "{frame_dir}/%06d.jpg"'
-    os.system(cmd)
+    cmd = ['ffmpeg',
+           '-i', f'{video}', '-ss', '00:00:0',
+           '-r', f'{decode_rate}', '-f', 'image2', f'{frame_dir}/%06d.jpg']
+    ffmpeg_subprocess(cmd)
+
+
+def extract_audio(video, audio_dir):
+    cmd = ['ffmpeg',
+           '-i', f'{video}', '-vn', '-acodec', 'copy',
+           f'{audio_dir}.aac']
+    ffmpeg_subprocess(cmd)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Extract frames')
-    parser.add_argument('--root', type=str, help='root directory')
+    parser = argparse.ArgumentParser(description='Extract frames and audios')
+    parser.add_argument('--root', type=str, default='/mldisk/nfs_shared_/sy/sea_story')
     parser.add_argument('--video_dir', type=str, default='videos')
+    parser.add_argument('--audio_dir', type=str, default='audios')
     parser.add_argument('--frame_dir', type=str, default='frames')
     parser.add_argument('--fps', type=int, default=1)
 
     args = parser.parse_args()
-    args.root = '/mldisk/nfs_shared_/sy/sea_story'
 
     video_dir = os.path.abspath(os.path.join(args.root, args.video_dir))
+    audio_dir = os.path.abspath(os.path.join(args.root, args.audio_dir))
     frame_dir = os.path.abspath(os.path.join(args.root, args.frame_dir))
 
     log = os.path.join(args.root, f'extract_{args.frame_dir}.log')
@@ -35,17 +53,25 @@ if __name__ == '__main__':
         logger.info(f'Frame directory {frame_dir} is already exist')
         exit(1)
 
-    videos = sorted(
-        [os.path.relpath(os.path.join(r, v), video_dir) for r, d, f in os.walk(video_dir, followlinks=True) for v in f
-         if v.split('.')[1].lower() in VIDEO_EXTENSION])
+    if os.path.exists(audio_dir) and len(os.listdir(audio_dir)) != 0:
+        logger.info(f'Audio directory {audio_dir} is already exist')
+        exit(1)
+
+    video_cls = ['HighLight', 'Origin']
+    videos = sorted([os.path.join(c, v) for c in video_cls for v in os.listdir(os.path.join(video_dir, c))])
 
     logger.info(f'Decode {len(videos)} videos, {args.fps} fps')
     logger.info(f'Videos ... {video_dir}')
     logger.info(f'Frames ... {frame_dir}')
+    logger.info(f'Audios ... {audio_dir}')
     logger.info(f'log ... {os.path.abspath(log)}')
 
     bar = tqdm.tqdm(videos, mininterval=1, ncols=150)
 
+    for c in video_cls:
+        os.makedirs(os.path.join(audio_dir, c))
+
     for v in bar:
         os.makedirs(os.path.join(frame_dir, v))
         extract_frame(os.path.join(video_dir, v), os.path.join(frame_dir, v), args.fps)
+        extract_audio(os.path.join(video_dir, v), os.path.join(audio_dir, v))
